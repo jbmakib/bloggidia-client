9ce8a46df737d15cff321499557599e17216f1e8
jbmakib <mdfoujolmobin@gmail.com>
2021-12-22 21:44:22 +0600

    updated

--- src/utilities/Firebase/firebase.init.js
+++ src/utilities/Firebase/firebase.init.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase.config";
 
 const initializeFirebaseApp = () => {
-    console.log(firebaseConfig);
     initializeApp(firebaseConfig);
 };
 
